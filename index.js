af91a24db3d3445b427bd7f80be10fd1021f0b17
dennismuturi1@gmail.com <dennis@WIN-AVPLU2QPEMC.localdomain>
2022-08-03 14:27:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const copyofCats = [...cats,name];
+    return copyofCats;
+}
+
+function prependCat(name){
+    const copyofCats = [name,...cats];
+    return copyofCats;
+}
+
+function removeLastCat(){
+     return cats.slice(0,-1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
+
